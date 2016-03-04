e80360381296ab14d19c2623bed679940375f12a
Slavik1985 <sleshnfs01@gmail.com>
2016-03-05 00:22:50 +0200

    last modification 0503 2016 00-23

--- SpringStart2/src/main/java/com/entity/Role.java
+++ SpringStart2/src/main/java/com/entity/Role.java
@@ -4,6 +4,9 @@ public enum Role {
 
 	USER,ADMIN,GUEST;
 	
+	Role(){
+		
+	}
 	@Override
 	public String toString() {
 		return "RoleTYPE [id=" + ordinal() + ", Role - " + name() + "]";
--- SpringStart2/src/main/java/com/entity/Sex.java
+++ SpringStart2/src/main/java/com/entity/Sex.java
@@ -3,6 +3,9 @@ package com.entity;
 public enum Sex {
 	MALE,FEMALE;
 	
+	Sex(){
+		
+	}
 	@Override
 	public String toString() {
 		return "sexTYPE [id=" + ordinal() + ", SEX - " + name() + "]";
